997ad1a2d62a8b32449a03959f7b9b4d9b26bea2
Noud Verstijnen <noud.verstijnen@gmail.com>
2021-01-10 17:34:14 +0100

    first implementation of simple weather station

--- weatherstation.ino
+++ weatherstation.ino
@@ -1,9 +1,15 @@
 #include <LiquidCrystal.h>
+#include <dht.h>
+
+dht DHT;
 
 // LiquidCrystal(RS, E, D4, D5, D6, D7)
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+#define DHT11_PIN 7
+
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("Temperature");
   lcd.setCursor(14,0);
@@ -16,5 +22,13 @@ void setup() {
 }
 
 void loop() {
+  int chk = DHT.read11(DHT11_PIN);
+  int temp = DHT.temperature;
+  int hum = DHT.humidity;
+  lcd.setCursor(12,0);
+  lcd.print(temp);
 
+  lcd.setCursor(12,1);
+  lcd.print(hum);
+  delay(5000);
 }
